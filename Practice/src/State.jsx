c510e02ccba6c9bf5796fe40e7660947cc3460a0
dhrupad777 <dhrupadrajpurohit@gmail.com>
2025-02-11 23:18:40 +0530

    Today i also learnt how to work with adding array elements to the frontend

--- Practice/src/State.jsx
+++ Practice/src/State.jsx
@@ -5,6 +5,9 @@ export default function State(){
 
 const [count, setCount]= React.useState(0);
 const [toogle,setToogle]=React.useState(false);
+const [emoji,setEmoji]=React.useState([])
+const allEmojis = ["😎", "✨", "🚀", "🎸", "🎨", "🎮", "🍕", "🍦", "🌴", "🏔️", "🌌", "💡", "🔥", "💯", "💎", "🦄", "🌈", "🦊", "🦉", "🐾"]
+const reactallEmojies = emoji.map(x => <p key={x}>{x}</p>)
 
 function add(){
     setCount(x => x + 1)//when new value is determined by new value it is advised to use a callback function used here
@@ -15,6 +18,10 @@ function subtract(){
 function tooglefunc(){
     // setToogle(toogle ? false : true) // now we could do this but the new value depends on ou old value so foe better convention
     setToogle(x => !x)
+}
+function addEmoji(){
+    // setEmoji(x => (x.length < 2 ? ["😎", "✨"] : ["😎", "✨", ...x.slice(2), allEmojis[x.length ]]));
+    setEmoji(x => [...x,allEmojis[x.length]])
 }
 
     return(
@@ -31,6 +38,17 @@ function tooglefunc(){
 
 <h1>Below is a toogle function</h1>
 <button onClick={tooglefunc} aria-label={`the current value is ${toogle ? "yes":"no"}. click this to chance this to ${!toogle}`}>{toogle ? "yes":"no"}</button>
+
+
+<h1>
+    below you can tap and get emojies
+</h1>
+<button onClick={addEmoji}>
+😎?
+</button>
+<p className="emocontainer">
+    {reactallEmojies}
+</p>
 
 
 </>
