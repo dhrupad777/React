408c38a563dc5f84e15790b40107dd45994e86e1
dhrupad777 <dhrupadrajpurohit@gmail.com>
2025-02-14 21:48:54 +0530

    practicing Conditional rendering

--- Practice/src/Quizcard.jsx
+++ Practice/src/Quizcard.jsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
 import "./Quizcard.css";
 
 export default function Quizcard(props) {
-    const [showAnswer, setShowAnswer] = useState(false);
-
     return (
-        <>
-            <h1 id="que">
-                {props.data.question}
-            </h1>
-            <button onClick={() => setShowAnswer(!showAnswer)}>
-                {showAnswer ? "Hide Answer" : "Show Answer"}
-            </button>
-            {showAnswer && (
-                <h1 id="ans">
-                    {props.data.answer}
-                </h1>
-            )}
-        </>
+        <div className="Quizcard">
+            <h1 id="que">{props.data.question}</h1>
+            <button>Answer</button>
+            <h1 id="ans">{props.data.answer}</h1>
+        </div>
     );
 }
